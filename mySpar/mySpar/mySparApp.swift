bf68a387466056ba5d4fe7a188b9577616b6fe3e
Радик Хазиев <121854191+Croleack@users.noreply.github.com>
2023-08-13 16:17:55 +0500

    let's go

---
+++ mySpar/mySpar/mySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySparApp.swift
+//  mySpar
+//
+//  Created by Enzhe Gaysina on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct mySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
