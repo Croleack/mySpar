47351d58dd1183345747c68987f82a2e4b982d24
Радик Хазиев <121854191+Croleack@users.noreply.github.com>
2023-08-13 16:42:51 +0500

    step 1 HomePageView

---
+++ mySpar/Components/HomePageView.swift
@@ -0,0 +1,40 @@
+//
+//  HomePageView.swift
+//  mySpar
+//
+//  Created by Enzhe Gaysina on 13.08.2023.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    @State private var location: String = ""
+    var body: some View {
+        
+        HStack{
+            HStack(spacing: 0) {
+                Image("Лок")
+                    .resizable()
+                    .frame(width: 15, height: 25)
+                    .padding(.trailing)
+                
+                TextField("Москва, Москва и Московская область", text: $location)
+                    .font(.title3)
+                    .foregroundColor(.white)
+            }.padding()
+            
+            
+            Button(action: {}) {
+                Image(systemName: "list.dash")
+                    .imageScale(.large)
+                    .foregroundColor(.green)
+                    .padding(.trailing)
+            }
+        }
+    }
+}
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
